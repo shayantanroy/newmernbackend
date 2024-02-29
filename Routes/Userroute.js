516c1b8d5cce903a91dedf6ef92fed0ef8fbf3ea
shayantanroy <shayantanroy100@gmail.com>
2024-02-29 14:41:49 +0530

    update userroute

--- Routes/Userroute.js
+++ Routes/Userroute.js
@@ -12,9 +12,14 @@ router.get("/googlelogin",passport.authenticate("google",
 
 router.get(
     "/login",
-    passport.authenticate("google", {
-      successRedirect: process.env.FRONTEND_URL,
-    })
+    passport.authenticate("google", 
+    (req,res,next)=>{
+      res.send("logged in");
+    }
+    // {
+    //   successRedirect:process.env.FRONTEND_URL,
+    // }
+    )
   );
 
 router.get("/me",isAuthenticated,myprofile);
