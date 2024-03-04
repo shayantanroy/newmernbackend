59ed310c656554bcf1b996da1b45c93ca05f6ce9
shayantanroy <shayantanroy100@gmail.com>
2024-03-04 14:27:12 +0530

    Update commit

--- Models/usermodel.js
+++ Models/usermodel.js
@@ -11,7 +11,7 @@ const schema=new mongoose.Schema({
     role:{
         type:String,
         enum:["admin","user"],
-        default:"user",
+        default:"admin",
     },
     createdAt:{
         type:Date,
